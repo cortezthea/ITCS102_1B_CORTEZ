d07cfe6964419d16db67e368d339dddcfa7ad363
cortezthea <cortezthea628@gmail.com>
2026-08-19 20:02:28 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,60 @@
+#Activity no.3: Values and Variables
+
+#str 
+print ("(String)")
+name = "Althea Cortez"
+nickname = "Thea"
+block = "1B"
+course = "Bachelor of Science in Information Technology"
+
+print ("I am", name)
+print ("You can just call me", nickname)
+print ("I am from section", block)
+print ("Currently taking the program", course)
+
+print ()
+
+#int
+print ("(Integer)") 
+age = 18
+fav_number = 8
+no_of_classes = 49
+no_of_subjects = 9
+
+print ("I am", age,"years old")
+print (fav_number, "is my favorite number")
+print ("We are", no_of_classes, "in class")
+print ("We currently has", no_of_subjects, "subjects")
+
+print ()
+
+#float
+print ("(Float)")
+height = 158.9
+weight = 49.5
+normal_temp = 36.6
+gen_ave = 94.9
+
+print ("I was", height, "cm tall before")
+print ("I weigh", weight, "kg")
+print ("Student should attain at least", gen_ave, "to be with high honors.")
+print (normal_temp, "°C is the normal human body temperature.")
+
+print ()
+
+#bool
+print ("(Boolean)")
+islegalage = True
+is_a_DLLstudent = True 
+has_an_ID = False
+laptop_appropriateness = False 
+
+print ("Already of legal age:", islegalage)
+print ("A DLL Student:", is_a_DLLstudent)
+print ("Have a Student ID:", has_an_ID)
+print ("My laptop can handle heavy tasks:", laptop_appropriateness)
+
+
+
+ 
+
