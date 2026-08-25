b747c0db530a55321bed96cf5a7d1dbd8dfabb2f
cortezthea <cortezthea628@gmail.com>
2026-08-26 00:13:07 +0800

    Add files via upload

---
+++ code_challenge1.py
@@ -0,0 +1,7 @@
+#Code Challenge No.1
+
+
+print (
+	"\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\tHi!My name is Althea\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t  I'm from BSIT-1B  \t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**********************************************************************************\n\t\t\t\t\t**********************************************************************************"
+)
+
