f536619b67759bbdb9117a5c7ee31176342d8310
cortezthea <cortezthea628@gmail.com>
2026-08-25 21:42:27 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,12 @@
+#Activity 4 (Escape Sequence) 
+
+# \n -new line 
+# \t -tab space
+# \r -carriage return/deprecated (obsolete)
+# \b -back space 
+# \\ -insert lash
+# \" -insert double quotation
+
+phrase1 = "\t\"Everything happens for you and not against you.\" \b\n\t\t\"God is within  her, she will not fall; God will help her at break of day.\"\n\t\t\t\t\t\t -Psalm 46:5-"
+
+print (phrase1)
