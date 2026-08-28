950cdfccfafb42fc6a4f63115da5f89641001ba4
cortezthea <cortezthea628@gmail.com>
2026-08-28 22:41:16 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,27 @@
+#Activity no.5 (Operators activity)
+
+a = 8
+b = 6 
+
+sum = a+b 
+difference = a - b 
+product = a * b
+quotient = a / b
+modulus = a % b 
+floor_division = a // b
+exponential = a ** b  
+
+print ("\"Use of Operators\"")
+print ()
+print ("The sum of", a, "and", b, "is", sum)
+print ("The difference of", a, "and", b, "is", difference)
+print ("The product of", a, "and", b, "is", product)
+print ("The quotient of", a, "and", b, "is", quotient)
+print ("The modulo of", a, "and", b, "is", modulus)
+print ("The floor division of", a, "and", b, "is", floor_division)
+print ("The exponentiation of", a, "and", b, "is", exponential)
+
+
+
+
+
