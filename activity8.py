a58a85c77ab26ef1c17d7ca56e612626b758e68d
cortezthea <cortezthea628@gmail.com>
2026-09-05 16:18:14 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,24 @@
+#Activity no. 8 (Empty String) 
+
+hobbies = ""
+
+h = input ("Enter your hobby:")
+hobbies += h + ","
+
+h = input ("What else?")
+hobbies += h + ","
+
+h = input ("What else?")
+hobbies += h + ","
+
+h = input ("What else?")
+hobbies += h + ","
+
+h = input ("What else?")
+hobbies += h + ","
+
+print ("Your hobbies are {",hobbies,"}.")
+print ("May you always enjoy doing your hobbies!")
+
+
+
