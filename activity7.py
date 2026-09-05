790b816af3c8fa85fdafee16cddd2f0d978d17d7
cortezthea <cortezthea628@gmail.com>
2026-09-05 15:55:37 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,41 @@
+#Activity no. 7(Assignment Operators)
+
+total = 18
+
+
+num1 = eval(input("Enter your 1st number: "))
+total += num1 
+
+num2 = eval(input("Enter your 2nd number: "))
+total -= num2
+
+num3 = eval(input("Enter your 3rd number: "))
+total *= num3 
+ 
+num4 = eval(input("Enter your 4th number: "))
+total /= num4
+
+num5 = eval(input("Enter your 5th number: "))
+total += num5
+
+num6 = eval(input("Enter your 6th number: "))
+total -= num6
+
+num7 = eval(input("Enter your 7th number: "))
+total -= num7
+
+num8 = eval(input("Enter your 8th number: "))
+total *= num8
+
+num9 = eval(input("Enter your 9th number: "))
+total += num9 
+
+num10 = eval(input("Enter your 10th number: "))
+total /= num10
+
+print ("The total of the numbers you provided is",total)
+
+
+
+
+
