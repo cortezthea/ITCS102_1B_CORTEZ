3bceed35dc8e6bc936386509be67bdb22c2675e1
cortezthea <cortezthea628@gmail.com>
2026-09-08 21:30:42 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,15 @@
+#Activity no.10 (Basic if else programming) 
+
+#and (logical operator)
+
+username = "altheaAC"
+password = "cortez18"
+
+u= input ("Username:")
+p= input ("Enter your password:")
+
+if u == username and p == password:
+	print ("Username and password correct")
+
+else: 
+	print ("Incorrect.Recheck your username and password.")
---
+++ activity9.py
@@ -0,0 +1,25 @@
+#Activity no.9 (Relational Operator) 
+
+# >  greater than 
+# <  less than 
+# >= greater than or equal 
+# <= less than or equal 
+# == equal 
+# != not equal
+
+x = 8
+y = 6
+
+password_1 = 123456
+password_2 = 246810
+
+print (x > y)
+print (x < y)
+print (x >= y)
+print (x <= y)
+print (x == y)
+print (x != y)
+print (password_1 == password_2)
+print (password_1 != password_2)
+
+
