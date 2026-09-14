425ea021c8f2f763bff2f4631659e6f507f76d51
cortezthea <cortezthea628@gmail.com>
2026-09-14 23:21:16 +0800

    Add files via upload

---
+++ activity_14.py
@@ -0,0 +1,16 @@
+#Activity no.14 (Business)
+
+# age (integer)
+# is_employed (boolean)
+# credit_score (integer)
+# annual_income (float)
+# has_collateral (boolean)
+
+age = int (input("Enter your age: "))
+is_employed = bool (input("Are you currently employed --> "))
+credit_score = eval (input("Enter your credit score history: "))
+annual_income = eval (input("Enter your annual income: "))
+has_collateral = bool (input("Do you have any collateral --> "))
+
+#Business Logic and Results
+# Baseline eligibility 
