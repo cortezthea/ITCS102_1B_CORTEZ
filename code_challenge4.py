19711b9a7d190f155ab076c679e079e6504423e3
cortezthea <cortezthea628@gmail.com>
2026-09-19 10:35:37 +0800

    Update code_challenge4.py

--- code_challenge4.py
+++ code_challenge4.py
@@ -1,4 +1,4 @@
-#Activity no.13 (Bank loan and interset rate approver)
+# Activity no. 13 (Bank loan and interest rate approver)
 
 # age (integer)
 # is_employed (boolean)
@@ -6,61 +6,129 @@
 # annual_income (float)
 # has_collateral (boolean)
 
-#need login 
-#username, password
-#loanee first name and job description 
+# need login
+# username, password
+# loanee first name and job description
 
-#prompt user to enter name/description of collateral e.g motorcycle, land, house 
-#prompt user the value of the collateral anything less than 30k is invalid 
+# prompt user to enter name/description of collateral
+# prompt user the value of the collateral anything less than 30k is invalid
 
-#maximum age for loan is 65 
-#ask user amount to loan, and the calculated interest rate using base rate 
+# maximum age for loan is 65
+# ask user amount to loan, and the calculated interest rate using base rate
 
-#LOGIN 
-print ("============== LOG IN ==============")
+# LOGIN
+print("============== LOG IN ==============")
 import getpass
+
 username = "altheaAC"
 password = "18"
 
-u = input ("Enter username:")
-p = getpass.getpass ("Enter your password:")
+u = input("Enter username: ")
+p = getpass.getpass("Enter your password: ")
 
 if u == username and p == password:
-	print ("Username and password correct. Let's proceed to the next part.")
-     
-    print ("============ LOANEE'S INFORMATION ============")
+    print("Username and password correct. Let's proceed to the next part.")
 
-    name = input ("Enter complete name: ")
-    job = input ("Enter your Job Description ")
+    print("============ LOANEE'S INFORMATION ============")
+    name = input("Enter complete name: ")
+    job = input("Enter your Job Description: ")
 
-#=======================================================================================
-    age = int (input("Enter your age: "))
+    print(".................................")
+
+    has_collateral = input("Do you have any collateral? ") == "True"
+    if has_collateral == True:
+        name_collateral = input("What is your collateral (description): ")
+
+        collateral_value = float(input("Enter the value of the collateral: "))
+        if collateral_value < 30000:
+            print("The value of the collateral must not be less than 30 000")
+        else:
+            print("Collateral accepted")
+
+    #=======================================================================================
+
+    age = int(input("Enter your age: "))
     is_employed = input("Are you currently employed --> ") == "True"
-    crdt_score = eval (input("Enter your credit score history: "))
-    annual_income = eval (input("Enter your annual income: "))
-    has_collateral= input("Do you have any collateral --> ") == "True"
-    print (".................................")
+    crdt_score = eval(input("Enter your credit score history: "))
+    annual_income = eval(input("Enter your annual income: "))
 
-# Business Logic and Results
-# Baseline eligibility 
-# Financial Evaluation
+    print(".................................")
 
-    if age >= 21 and is_employed == True: #Tier 1
-        if crdt_score >= 750:
+    # Business Logic and Results
+    # Baseline eligibility
+    # Financial Evaluation
+
+    if 21 <= age <= 65 and is_employed == True:         
+        if crdt_score >= 750: #Tier 1
             base_interest_rate = 5.0
             if annual_income >= 100000:
                 base_interest_rate = 4.5
-            print ("Your final rate is", base_interest_rate)
+            print("Your final rate is", base_interest_rate, "%")
+
+            # LOAN
+            amount_to_loan = float(input("Enter amount to loan: "))
+            # INTEREST VALUE
+            interest = amount_to_loan * (base_interest_rate / 100)
+            total_amount = amount_to_loan + interest
+
+            print(".................................")
+            print("Loan Amount:", amount_to_loan)
+            print("Interest Rate:", base_interest_rate, "%")
+            print("Interest:", interest)
+            print("TOTAL AMOUNT:", total_amount)
 
         elif 600 <= crdt_score < 750: #Tier 2
             base_interest_rate = 8.0
-            if has_collateral == True:
+            if has_collateral == True and collateral_value >= 30000:
                 base_interest_rate = 7.0
+                print("Your final rate is", base_interest_rate, "%")
+
+                # LOAN
+                amount_to_loan = float(input("Enter amount to loan: "))
+                # INTEREST VALUE
+                interest = amount_to_loan * (base_interest_rate / 100)
+                total_amount = amount_to_loan + interest
+
+                print(".................................")
+                print("Loan Amount:", amount_to_loan)
+                print("Interest Rate:", base_interest_rate, "%")
+                print("Interest:", interest)
+                print("TOTAL AMOUNT:", total_amount)
             elif annual_income < 40000:
                 base_interest_rate = 9.5
-        print ("Your final rate is", base_interest_rate,"%")
+                print("Your final rate is", base_interest_rate, "%")
+
+                # LOAN
+                amount_to_loan = float(input("Enter amount to loan: "))
+                # INTEREST VALUE
+                interest = amount_to_loan * (base_interest_rate / 100)
+                total_amount = amount_to_loan + interest
+
+                print(".................................")
+                print("Loan Amount:", amount_to_loan)
+                print("Interest Rate:", base_interest_rate, "%")
+                print("Interest:", interest)
+                print("TOTAL AMOUNT:", total_amount)
+            else:
+                print("Your final rate is", base_interest_rate, "%")
+
+                # LOAN
+                amount_to_loan = float(input("Enter amount to loan: "))
+                # INTEREST VALUE
+                interest = amount_to_loan * (base_interest_rate / 100)
+                total_amount = amount_to_loan + interest
+
+                print(".................................")
+                print("Loan Amount:", amount_to_loan)
+                print("Interest Rate:", base_interest_rate, "%")
+                print("Interest:", interest)
+                print("TOTAL AMOUNT:", total_amount)
 
         elif crdt_score < 600: #Tier 3
-            print ("Rejected: Credit score too low")
-    else: 
-        print ("Rejected: Fails baseline criteria")
+            print("Rejected: Credit score too low")
+
+    else:
+        print("Rejected: Fails baseline criteria")
+
+else:
+    print("Incorrect. Type your username and password again.")
